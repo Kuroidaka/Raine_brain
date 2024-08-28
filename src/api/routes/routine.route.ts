052addd6fc8e331761d9436ce4fa493051d723c7
Kuroidaka <pcanhgm@gmail.com>
2024-08-29 01:12:53 +0700

    feat: sync data into google calendar event when CURD routine

--- src/api/routes/routine.route.ts
+++ src/api/routes/routine.route.ts
@@ -3,14 +3,15 @@ import { validateDto } from '~/api/middlewares';
 import { CreateRoutineDto, UpdateRoutineDto } from '~/dto/routine.dto';
 import validateToken from '../middlewares/validate_token';
 import { routineController } from '../controllers/routine.controller';
+import { validateGgTokenOptional } from '../middlewares/validateGoogleTokenOp';
 
 const router = Router();
 
-router.post('/create', validateToken, validateDto(CreateRoutineDto), routineController.createRoutine);
+router.post('/create', validateToken, validateGgTokenOptional, validateDto(CreateRoutineDto), routineController.createRoutine);
 router.get('/get', validateToken, routineController.getRoutines);
 router.get('/get/:id', validateToken, routineController.getRoutineById);
-router.patch('/update/:id', validateToken, validateDto(UpdateRoutineDto), routineController.updateRoutine);
+router.patch('/update/:id', validateToken, validateGgTokenOptional, validateDto(UpdateRoutineDto), routineController.updateRoutine);
 router.patch('/toggle-status/:id', validateToken, routineController.toggleRoutineStatus);
-router.delete('/delete/:id', validateToken, routineController.deleteRoutine);
+router.delete('/delete/:id', validateToken, validateGgTokenOptional, routineController.deleteRoutine);
 
 export default router;
